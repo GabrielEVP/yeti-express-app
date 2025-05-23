0133a2c18f50ead2fd17578ef2bdd4547bacf246
Gabriel Vargas <gabrielenriquevargas2001@gmail.com>
2025-05-23 21:09:48 +0200

    refactor(couriers): improve courier details display and enhance payment calculations

--- src/components/dashboard/billing/TableBilling.vue
+++ src/components/dashboard/billing/TableBilling.vue
@@ -1,34 +1,41 @@
 <template>
    <Card>
-      <Table class="caption-bottom text-sm" v-if="data.length > 0">
-         <TableHead>
-            <TableRow class="bdata-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-               <TableContent>Número</TableContent>
-               <TableContent>Fecha</TableContent>
-               <TableContent class="text-center">Importe</TableContent>
-               <TableContent class="text-center">Estado</TableContent>
-               <TableContent class="text-center">Acciones</TableContent>
-            </TableRow>
-         </TableHead>
-         <TableBody>
-            <TableRow v-for="dataItem in data" :key="dataItem.id">
-               <TableContent class="text-gray-900 dark:text-gray-50">{{ dataItem.number }}</TableContent>
-               <TableContent>{{ formatDateCustom(dataItem.date) }}</TableContent>
-               <TableContent class="text-right">{{ dataItem.total }} $</TableContent>
-               <TableContent class="text-center">
-                  <Bagde :class="[getStatusBillingClass(dataItem.status)]">
-                     {{ getStatusBillingText(dataItem.status) }}
-                  </Bagde>
-               </TableContent>
-               <TableContent class="text-center">
-                  <div class="flex justify-center gap-2">
-                     <EyeButton :id="String(dataItem.id)" :route="url" />
-                     <DownloadButton @click="handleDownload(dataItem.id)" />
-                  </div>
-               </TableContent>
-            </TableRow>
-         </TableBody>
-      </Table>
+      <div class="max-h-96 overflow-y-auto" v-if="data.length > 0">
+         <Table class="caption-bottom text-sm w-full">
+            <TableHead>
+               <TableRow class="transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                  <TableContent>Número</TableContent>
+                  <TableContent>Fecha</TableContent>
+                  <TableContent class="text-center">Importe</TableContent>
+                  <TableContent class="text-center">Estado</TableContent>
+                  <TableContent class="text-center">Acciones</TableContent>
+               </TableRow>
+            </TableHead>
+
+            <TableBody>
+               <TableRow v-for="dataItem in data" :key="dataItem.id">
+                  <TableContent class="text-gray-900 dark:text-gray-50">
+                     {{ dataItem.number }}
+                  </TableContent>
+                  <TableContent>
+                     {{ formatDateCustom(dataItem.date) }}
+                  </TableContent>
+                  <TableContent class="text-right"> {{ dataItem.total }} $ </TableContent>
+                  <TableContent class="text-center">
+                     <Bagde :class="[getStatusBillingClass(dataItem.status)]">
+                        {{ getStatusBillingText(dataItem.status) }}
+                     </Bagde>
+                  </TableContent>
+                  <TableContent class="text-center">
+                     <div class="flex justify-center gap-2">
+                        <EyeButton :id="String(dataItem.id)" :route="url" />
+                        <DownloadButton @click="handleDownload(dataItem.id)" />
+                     </div>
+                  </TableContent>
+               </TableRow>
+            </TableBody>
+         </Table>
+      </div>
       <EmptyData v-else />
    </Card>
 </template>
