9d1fd4a3a2a49cf95cbc49e6ffb2c02cf99d2b01
Gabriel Vargas <gabrielenriquevargas2001@gmail.com>
2025-05-27 15:53:29 +0200

    feat(deliveries): remove unused LinesDropdown and LinesForm components; update index file for cleaner exports

--- src/views/deliveries/components/index.ts
+++ src/views/deliveries/components/index.ts
@@ -1,16 +1,13 @@
-export { default as Details } from "./Details.Delivery.vue";
-export { default as Form } from "./Form.Delivery.vue";
-export { default as List } from "./List.Delivery.vue";
+export { default as Details } from './Details.Delivery.vue';
+export { default as Form } from './Form.Delivery.vue';
+export { default as List } from './List.Delivery.vue';
 
-export { default as DeliveryReceiptDropdown } from "./delivery-data/Receipt/ReceiptDropdown.Delivery.vue";
+export { default as DeliveryReceiptDropdown } from './delivery-data/Receipt/ReceiptDropdown.Delivery.vue';
 
-export { default as DeliveryClientAddressList } from "./delivery-data/clients/ClientAddressList.Delivery.vue";
-export { default as DeliveryClientModalForm } from "./delivery-data/clients/ClientModalForm.Delivery.vue";
+export { default as DeliveryClientAddressList } from './delivery-data/clients/ClientAddressList.Delivery.vue';
+export { default as DeliveryClientModalForm } from './delivery-data/clients/ClientModalForm.Delivery.vue';
 
-export { default as DeliveryCourierPaymentsDropdown } from "./delivery-data/courier-payment/CourierPaymentsDropdown.Delivery.vue";
-export { default as DeliveryCourierPaymentsForm } from "./delivery-data/courier-payment/CourierPaymentsForm.Delivery.vue";
+export { default as DeliveryCourierPaymentsDropdown } from './delivery-data/courier-payment/CourierPaymentsDropdown.Delivery.vue';
+export { default as DeliveryCourierPaymentsForm } from './delivery-data/courier-payment/CourierPaymentsForm.Delivery.vue';
 
-export { default as DeliveryLinesForm } from "./delivery-data/lines/LinesForm.Delivery.vue";
-export { default as DeliveryLinesDropdown } from "./delivery-data/lines/LinesDropdown.Delivery.vue";
-
-export { default as DeliveryPaymentsDrowdown } from "./delivery-data/delivery-payment/PaymentsDrowdown.Delivery.vue";
+export { default as DeliveryPaymentsDrowdown } from './delivery-data/delivery-payment/PaymentsDrowdown.Delivery.vue';
