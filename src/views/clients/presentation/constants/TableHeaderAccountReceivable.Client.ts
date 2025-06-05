f5737a303cbeb5917a20b49e2d12047d21332429
Gabriel Vargas <gabrielenriquevargas2001@gmail.com>
2025-06-05 17:07:22 +0200

    repair merge

--- src/views/clients/presentation/constants/TableHeaderAccountReceivable.Client.ts
+++
@@ -1,6 +0,0 @@
-export const TABLE_HEADER_ACCOUNT_RECEIVABLE = [
-  { label: 'Nombre legal', key: 'legal_name', position: 'center', sortable: true },
-  { label: 'Tipo de documento', key: 'type', position: 'center', sortable: true },
-  { label: 'Numero de documento', key: 'registration_number', position: 'center', sortable: true },
-  { label: 'Restante a pagar', key: 'registration_number', position: 'right', sortable: true },
-];
--- src/views/clients/presentation/router/Router.Client.ts
+++ src/views/clients/presentation/router/Router.Client.ts
@@ -1,9 +1,6 @@
 import { RouteRecordRaw } from 'vue-router';
 import { Details, Form, List } from '@views/clients/presentation/pages';
-<<<<<<< HEAD
-=======
 
->>>>>>> 3b3e9a1 (Remove client delivery debt functionality)
 export const ClientRouter: Array<RouteRecordRaw> = [
   {
     path: '/clients',
